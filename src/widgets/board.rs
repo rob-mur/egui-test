4554b5134b73a598687eb89743f988fb53dffac5
rob-mur <rmurphyswimmer@gmail.com>
2022-06-07 16:12:19 +0200

    minor edits to look

--- src/widgets/board.rs
+++ src/widgets/board.rs
@@ -1,4 +1,4 @@
-use egui::{Color32, Direction, Layout, Response, Stroke, Ui, Widget};
+use egui::{Color32, Direction, Frame, Layout, Response, Stroke, Ui, Widget};
 use egui_extras::{Size, Strip, StripBuilder};
 
 pub struct BoardWidget;
@@ -8,7 +8,8 @@ impl Widget for BoardWidget {
         egui::CentralPanel::default()
             .show_inside(ui, |ui| {
                 egui::CentralPanel::default().show_inside(ui, |ui| {
-                    ui.label("Next player: X");
+                    ui.heading("Next player: X");
+                    ui.label("");
                     board(ui);
                 });
             })
@@ -17,17 +18,31 @@ impl Widget for BoardWidget {
 }
 
 fn board(ui: &mut Ui) {
-    ui.style_mut().spacing.item_spacing.x = 0.0;
-    ui.style_mut().spacing.item_spacing.y = 0.0;
+    ui.style_mut().spacing.item_spacing.x = 2.5;
+    ui.style_mut().spacing.item_spacing.y = 2.5;
 
-    let mut idx = 0;
-    StripBuilder::new(ui)
-        .cell_layout(Layout::centered_and_justified(Direction::LeftToRight))
-        .sizes(Size::remainder(), 3)
-        .vertical(|mut strip| {
-            fill_strip(&mut strip, &mut idx);
-            fill_strip(&mut strip, &mut idx);
-            fill_strip(&mut strip, &mut idx);
+    egui::CentralPanel::default()
+        .frame(Frame {
+            inner_margin: Default::default(),
+            outer_margin: Default::default(),
+            rounding: Default::default(),
+            shadow: Default::default(),
+            fill: Color32::BLACK,
+            stroke: Stroke {
+                width: 5.0,
+                color: Color32::BLACK,
+            },
+        })
+        .show_inside(ui, |ui| {
+            let mut idx = 0;
+            StripBuilder::new(ui)
+                .cell_layout(Layout::centered_and_justified(Direction::LeftToRight))
+                .sizes(Size::remainder(), 3)
+                .vertical(|mut strip| {
+                    fill_strip(&mut strip, &mut idx);
+                    fill_strip(&mut strip, &mut idx);
+                    fill_strip(&mut strip, &mut idx);
+                });
         });
 }
 
@@ -43,14 +58,8 @@ fn fill_strip(strip: &mut Strip<'_, '_>, idx: &mut i32) {
 
 fn fill_cell(strip: &mut Strip<'_, '_>, idx: &mut i32) {
     strip.cell(|ui| {
-        ui.painter().rect_stroke(
-            ui.available_rect_before_wrap(),
-            0.0,
-            Stroke {
-                width: 5.0,
-                color: Color32::BLACK,
-            },
-        );
+        ui.painter()
+            .rect_filled(ui.available_rect_before_wrap(), 0.0, Color32::WHITE);
         ui.label(format!("{}", *idx));
         *idx += 1;
     });
